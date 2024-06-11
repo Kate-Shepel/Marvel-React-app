4897c8f6281a7b9c8d496ae5b8a784745b0d23fb
Kate-Shepel <kateborichevskaya@gmail.com>
2024-06-11 14:31:25 +0200

    feat: add marvel service to character list creation

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -90,20 +90,20 @@ class RandomChar extends Component {
         }
         return (
             <div className="randomchar__block">
-            <img src={thumbnail} style={imgStyle} alt="Random character" className="randomchar__img" />
-            <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">{description}</p>
-            <div className="randomchar__btns">
-                <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-                </a>
-                <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-                </a>
+                <img src={thumbnail} style={imgStyle} alt="Random character" className="randomchar__img" />
+                <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">{description}</p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                    <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                    <div className="inner">Wiki</div>
+                    </a>
+                </div>
+                </div>
             </div>
-            </div>
-        </div>
         )
     }
 
