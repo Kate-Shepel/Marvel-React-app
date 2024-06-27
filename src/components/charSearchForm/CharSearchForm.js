cf44bb6fb6f7230521b5d45992be9371c21fa0c1
Kate-Shepel <kateborichevskaya@gmail.com>
2024-06-27 22:55:53 +0200

    feat: add search error and search success logics

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -1,14 +1,16 @@
 import { Formik, Form, Field, ErrorMessage as FormikErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import {useState} from 'react';
+import { Link } from 'react-router-dom';
 import useMarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './charSearchForm.scss';
 
 const CharSearchForm = () => {
 
   const [char, setChar] = useState(null);
-  const {getCharByName, clearError} = useMarvelService();
+  const {error, loading, getCharByName, clearError} = useMarvelService();
 
   const onCharLoaded = (char) => {
       setChar(char);
@@ -21,7 +23,18 @@ const CharSearchForm = () => {
           .then(onCharLoaded);
   }
 
-    return (
+  const errorMessage = error ? <div className="char__search-critical-error"><ErrorMessage /></div> : null;
+  const result = !char ? null : char.length > 0 ? 
+                  <div className="char__search-wrapper">
+                    <div className="char__search-success">There is! Visit {char[0].name} page?</div>
+                    <Link to={`/characters/${char[0].id}`} className="button button__secondary">
+                      <div className="inner">To page</div>
+                    </Link>
+                  </div> : 
+                  <div className="char__search-error">
+                    The character was not found. Check the name and try again
+                  </div>;
+      return (
         <div className="char__search-form">
             <Formik
               initialValues = {{
@@ -43,14 +56,17 @@ const CharSearchForm = () => {
                     type='text' 
                     placeholder="Enter name"/>
                   <button 
-                      type='submit' 
-                      className="button button__main">
-                      <div className="inner">find</div>
+                    type='submit' 
+                    className="button button__main"
+                    disabled={loading}>
+                    <div className="inner">find</div>
                   </button>
                 </div>
                 <FormikErrorMessage component="div" className="char__search-error" name="charName" />
               </Form>
             </Formik>
+            {errorMessage}
+            {result}
         </div>
     )
 }
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -21,7 +21,7 @@ const useMarvelService = () => {
 
   const getCharByName = async (name) => {
     const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
-    return _transformCharacter(res.data.results[0]);
+		return res.data.results.map(_transformCharacter);
   }
 
   const getComics = async (offset = _baseComicsOffset) => {
