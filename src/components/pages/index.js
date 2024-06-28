8eaffcce44726c66b657ea510aea43fb3c90eb3c
Kate-Shepel <kateborichevskaya@gmail.com>
2024-06-28 14:33:38 +0200

    refactor: transfer main logics for similar single pages to one SinglePage

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,8 @@
 import ComicsPage from "./ComicsPage";
 import MainPage from "./MainPage";
 import Page404 from "./404Page";
-import SingleComicsPage from "./SingleComicsPage";
-import SingleCharPage from "./SingleCharPage";
+import SingleComicsPage from "./singleComicsPage/SingleComicsPage";
+import SingleCharPage from "./singleCharPage/SingleCharPage";
+import SinglePage from './SinglePage';
 
-export {MainPage, ComicsPage, Page404, SingleComicsPage, SingleCharPage};
+export {MainPage, ComicsPage, Page404, SingleComicsPage, SingleCharPage, SinglePage};
